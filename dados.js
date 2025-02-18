acb2213d5a4ea4f28210d06c98dd0178fba45158
ojoiM <thaisbp2010@hotmail.com>
2025-02-18 13:52:37 -0300

    Create dados.js

---
+++ dados.js
@@ -0,0 +1,18 @@
+{
+  "titulo": "Minhas Pesquisas",
+  "descricao": "Lista de vídeos e livros interessantes.",
+  "itens": [
+    {
+      "tipo": "Livro",
+      "nome": "O Nome do Vento",
+      "autor": "Patrick Rothfuss",
+      "link": "https://www.estantevirtual.com.br/sebos-e-livreiros/sebo-espaco-cultural-2?sellerId=1001527&idioma=ingles&sellers=1001527&page=3"
+    },
+    {
+      "tipo": "Vídeo",
+      "nome": "Como aprender programação",
+      "autor": "Canal Exemplo",
+      "link": "https://youtube.com/shorts/BSp9OaAKtrU?si=KnK6-vK3QEy3VDxf"
+    }
+  ]
+}
